98693124214f2c568fea5a244773c1e4bcc7480b
OPSolutions <opsolutions@OPSolutionss-MacBook-Pro.local>
2022-03-21 10:23:53 +0800

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,83 @@
 import logo from '../logo.svg';
 import '../App.css';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import {startAction} from '../actions/startAction';
 import {stopAction} from '../actions/stopAction';
 import {Search, ApiList} from './ApiSearchList';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 
-const mapStateToProps =  state => ({
-  rotating: state.rotateReducer.rotating,
-  api: state.apiListReducer.api,
-  searchTerm: state.searchReducer.searchTerm
-});
+// const mapStateToProps =  state => ({
+//   rotating: state.rotateReducer.rotating,
+//   api: state.apiListReducer.api,
+//   searchTerm: state.searchReducer.searchTerm
+// });
 
-const mapDispatchToProps = dispatch => ({
-  startAction: () => dispatch(startAction),
-  stopAction: () => dispatch(stopAction),
-  onSearch: searchTerm => dispatch({type: 'SEARCH', searchTerm})
-});
+// const mapDispatchToProps = dispatch => ({
+//   startAction: () => dispatch(startAction),
+//   stopAction: () => dispatch(stopAction),
+//   onSearch: searchTerm => dispatch({type: 'SEARCH', searchTerm})
+// });
 
 const applyFilter = searchTerm => api => api.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 
 function App( props) {
+  const rotating = useSelector(state => state.rotateReducer.rotating);
+  const api = useSelector(state => state.apiListReducer.api);
+  const searchTerm = useSelector(state => state.searchReducer.searchTerm);
+  const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h2>Welcome Redux Sample</h2>
+
+//         <img src={logo} className={"App-logo" + (rotating ? '' : 'App-logo-paused')} alt="logo"
+//         onClick={ () => dispatch(rotating ? dispatch(stopAction): dispatch(startAction))
+//   }
+//   />
+
+//         <p>Prop state {rotating ? "rotating" : "paused"}</p>
+
+//        <Search value={searchTerm} onSearch={onSearch => searchTerm => dispatch({type: 'SEARCH', searchTerm})}>
+//         <p>SEARCH</p>
+//         </Search>
+
+//         <ApiList list = {api.filter(applyFilter(props.searchTerm))}></ApiList>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+// // export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default App;
+
+return (
+  <div className="App">
+    <header className="App-header">
+
+      <h1>React Redux Sample</h1>
+      <img 
+        src={logo} 
+        className={"App-logo" + (rotating ? "" : "App-logo-paused")} 
+        alt="logo" 
+        onClick={
+          () => dispatch(rotating ? dispatch(stopAction) : dispatch(startAction))
+        }
+      />
+      <p>Prop state {rotating ? "rotating" : "paused"}</p>
       
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Welcome Redux Sample</h2>
+      <Search value={searchTerm} onSearch={searchTerm => dispatch({type: 'SEARCH_SET', searchTerm})}>
+        <p>Search</p>
+      </Search>
 
-        <img src={logo} className={"App-logo" + (props.rotating ? '' : 'App-logo-paused')} alt="logo"
-        onClick={props.rotating ? props.stopAction: props.startAction} />
-
-        <p>Prop state {props.rotating ? "rotating" : "paused"}</p>
-
-       <Search value={props.searchTerm} onSearch={props.onSearch}>
-        <p>SEARCH</p>
-        </Search>
-
-        <ApiList list = {props.api.filter(applyFilter(props.searchTerm))}></ApiList>
-      </header>
-    </div>
-  );
+      <ApiList list={api.filter(applyFilter(searchTerm))}></ApiList>
+    </header>
+  </div>
+);
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import configureStore from './stores/store';
+import {Routes,Route, Link, BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store = {configureStore} >
-    <App />
-    </Provider>,
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ 
+   <React.StrictMode>
+     <Provider store = {configureStore}>
+     <BrowserRouter>
+       <Link to = "/">Home</Link>
+      <Routes>
+        <Route path= "/" element={<App />} />
+      
+      </Routes>
+      </BrowserRouter>
+      </Provider>,
+    </React.StrictMode>,
+  
+  document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
